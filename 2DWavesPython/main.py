44c339b549aec7b9450d677cd9c2a254292e8fba
Adi Vatsyayana <adivats2000@gmail.com>
2026-01-19 18:38:13 +0000

    Added 2D waves in python

---
+++ 2DWavesPython/main.py
@@ -0,0 +1,98 @@
+import pygame
+import math
+import time
+
+C = 400
+TIME_STEP = 0.0005
+DISTANCE_STEP = 1
+FIELD_PER_PIXEL = 0.05
+SCREEN_WIDTH, SCREEN_HEIGHT = (1200, 800)
+fieldWidth = int(SCREEN_WIDTH * FIELD_PER_PIXEL)
+fieldHeight = int(SCREEN_HEIGHT * FIELD_PER_PIXEL)
+field = [[0]*fieldHeight for _ in range(fieldWidth)]
+oldField = [[0]*fieldHeight for _ in range(fieldWidth)]
+newField = [[0]*fieldHeight for _ in range(fieldWidth)]
+start_time = pygame.time.get_ticks() / 1000.0
+
+def computeFieldEquation(i, k):
+    global field, oldField, newField
+    amplitude = field[i][k]
+    oldAmplitude = oldField[i][k]
+    leftIndex = i-1
+    #if leftIndex < 0: leftIndex = fieldWidth - 1
+    rightIndex = i + 1
+    #if rightIndex > fieldWidth - 1: rightIndex = 0
+    upIndex = k-1
+    #if upIndex < 0: upIndex = fieldHeight - 1
+    downIndex = k + 1
+    #if downIndex > fieldHeight - 1: downIndex = 0
+
+    
+    if leftIndex < 0: leftAmplitude = 0
+    else: leftAmplitude = field[leftIndex][k]
+
+    if rightIndex > fieldWidth - 1: rightAmplitude = 0
+    else: rightAmplitude = field[rightIndex][k]
+
+    if upIndex < 0: upAmplitude = 0
+    else: upAmplitude = field[i][upIndex]
+    
+    if downIndex > fieldHeight - 1: downAmplitude = 0
+    else: downAmplitude = field[i][downIndex]
+
+    sourceAmount = 0
+    elapsed_time = pygame.time.get_ticks() / 1000.0 - start_time  # Use elapsed time
+    if i == fieldWidth // 2 and k == fieldHeight // 2:
+        elapsed_time = pygame.time.get_ticks() / 1000.0 - start_time
+        sourceAmount = 1 * math.sin(elapsed_time * 10)
+    
+    laplacian = (rightAmplitude + leftAmplitude + upAmplitude + downAmplitude - 4*amplitude) / (DISTANCE_STEP * DISTANCE_STEP)
+    
+    newAmplitude = 2 * amplitude - oldAmplitude + C*C*TIME_STEP*TIME_STEP*laplacian + sourceAmount
+    
+    if i == 15 and (k < (fieldHeight // 2) - 2 or k > (fieldHeight // 2) + 2): newAmplitude = 0
+    
+    newAmplitude *= 0.995
+    return newAmplitude
+
+def calculateNextField():
+    global field, oldField, newField
+    oldField = [row[:] for row in field]
+    field = [row[:] for row in newField]
+
+    for i in range(fieldWidth):
+        for k in range(fieldHeight):
+            newAmplitude = computeFieldEquation(i, k)
+            newField[i][k] = newAmplitude
+
+def drawField():
+    for x in range(fieldWidth):
+        for y in range(fieldHeight):
+            screenX = x/FIELD_PER_PIXEL
+            screenY = y/FIELD_PER_PIXEL
+            amplitude = abs(field[x][y])  # Get absolute value
+            if amplitude > 0.1:  # Only draw if visible
+                radius = min(5, int(amplitude * 5))  # Cap the radius
+                pygame.draw.circle(screen, (255, 255, 255), (int(screenX), int(screenY)), radius)
+
+pygame.init()
+
+screen =pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("2D waves")
+clock = pygame.time.Clock()
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill((0, 0, 0))
+    calculateNextField()
+    drawField()
+
+    pygame.display.flip()
+    clock.tick(60)
+
+pygame.quit()
+
---
+++ WaveRaytracer/Program.cs
@@ -0,0 +1,72 @@
+﻿using System.Numerics;
+using Raylib_cs;
+
+static class WaveRaytracer
+{
+    const int width = 800;
+    const int height = 600;
+    static List<Source> sources = new List<Source>();
+    const int numberRaysPerSource = 10;
+
+    static void Start()
+    {
+        sources.Add(new Source(new Vector2(0, 0), 100));
+    }
+
+    public static void Main()
+    {
+        Start();
+        Raylib.InitWindow(width, height, "Wave Raytracer");
+
+        while (!Raylib.WindowShouldClose())
+        {
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Color.White);
+            ComputeSources();
+            Raylib.EndDrawing();
+        }
+    }
+
+    static void ComputeSources()
+    {
+        foreach(Source source in sources)
+        {
+            Vector2 screenPosition = source.position;
+            screenPosition.X += width / 2;
+            screenPosition.Y += height/ 2;
+            Raylib.DrawCircle((int)screenPosition.X, (int)screenPosition.Y, 5f, Color.Red);
+            
+            for(int i = 0; i < numberRaysPerSource; i++)
+            {
+                double angle = 2*Math.PI/numberRaysPerSource * i;
+                double xDir = Math.Cos(angle);
+                double yDir = Math.Sin(angle);
+                Vector2 rayDir = Vector2.Normalize(new Vector2((float)xDir, (float)yDir));
+                Vector2 endScreenPos = screenPosition + rayDir * 500;
+                Raylib.DrawLine((int)screenPosition.X, (int)screenPosition.Y, (int)endScreenPos.X, (int)endScreenPos.Y, Color.Red);
+            }
+        }
+    }
+
+    static void ComputeRay(Vector2 startPosition, Vector2 direction, float length)
+    {
+        
+    }
+
+    static void DrawScreen()
+    {
+        
+    }
+}
+
+class Source
+{
+    public Vector2 position;
+    public float frequency;
+
+    public Source(Vector2 _position, float _frequency)
+    {
+        position = _position;
+        frequency = _frequency;
+    }
+}
