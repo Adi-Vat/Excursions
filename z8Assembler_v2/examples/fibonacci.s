de4e2dcff53d7c60609b3b469cd21ffc0d20c3a9
Adi Vatsyayana <adivats2000@gmail.com>
2026-02-02 00:16:57 +0000

    added comments

--- z8Assembler_v2/examples/fibonacci.s
+++ z8Assembler_v2/examples/fibonacci.s
@@ -1,12 +1,12 @@
-LD R0, 5
-LD R1, 1
-LD R2, 1
-LD R3, 0
+LD R0, 5    ; R0 = 5
+LD R1, 1    ; R1 = 1
+LD R2, 1    ; R2 = 1
+LD R3, 0    ; R3 = 0
 
 loop:
-    LD R3, R2
-    ADD R2, R1
-    LD R1, R3
+    LD R3, R2   ; R3 = R2
+    ADD R2, R1  ; R2 = R1
+    LD R1, R3   ; R1 = R3
 
-    DEC R0
-    JNZ loop
+    DEC R0  ; R0--
+    JNZ loop ; if(R0 != 0) goto loop
