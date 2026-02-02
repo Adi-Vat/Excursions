958f4646d3d8a27c58742e05d9f30663fc935929
Adi Vatsyayana <adivats2000@gmail.com>
2026-02-02 13:46:42 +0000

    updates fibonacci

--- z8Assembler_v2/examples/fibonacci.s
+++ z8Assembler_v2/examples/fibonacci.s
@@ -2,6 +2,7 @@ LD R0, 5    ; Loop counter
 LD R1, 1    ; Previous Fibonacci number
 LD R2, 1    ; Current Fibonacci number
 LD R3, 0    ; Temporary storage
+.var final_output 0 ; final value
 
 loop:
     LD R3, R2   ; Save current value
@@ -11,4 +12,6 @@ loop:
     DEC R0  ; Decrement counter
     JNZ loop ; Continue if counter != 0
 
-; Result: R2 = 13 (0x0D)
+STR @final_output, R2
+
+; Result: final_output = 13 (0x0D)
