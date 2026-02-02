178c465cd7d96e8a8bde4e4348388ec04c5efbf0
Adi Vatsyayana <adivats2000@gmail.com>
2026-02-02 00:26:08 +0000

    changed comments

--- z8Assembler_v2/examples/fibonacci.s
+++ z8Assembler_v2/examples/fibonacci.s
@@ -1,12 +1,14 @@
-LD R0, 5    ; R0 = 5
-LD R1, 1    ; R1 = 1
-LD R2, 1    ; R2 = 1
-LD R3, 0    ; R3 = 0
+LD R0, 5    ; Loop counter
+LD R1, 1    ; Previous Fibonacci number
+LD R2, 1    ; Current Fibonacci number
+LD R3, 0    ; Temporary storag
 
 loop:
-    LD R3, R2   ; R3 = R2
-    ADD R2, R1  ; R2 = R1
-    LD R1, R3   ; R1 = R3
+    LD R3, R2   ; Save current value
+    ADD R2, R1  ; Calculate next Fibonacci: R2 = R1 + R2
+    LD R1, R3   ; Shift: previous = old current value
 
-    DEC R0  ; R0--
-    JNZ loop ; if(R0 != 0) goto loop
+    DEC R0  ; Decrement counter
+    JNZ loop ; Continue if counter != 0
+
+; Result: R2 = 13 (0x0D)
