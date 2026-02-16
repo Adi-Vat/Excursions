5ddc7bd552e0ff43bd1ec1bf9b19dd44f273f7b2
Adi Vatsyayana <adivats2000@gmail.com>
2026-02-16 19:54:44 +0000

    Created Haptic Sim 2D

---
+++ HapticSim2D/Physics.cs
@@ -0,0 +1,95 @@
+
+using System.Numerics;
+using Raylib_cs;
+
+public class Physics
+{
+    // Returns the normal force applied on the ball from the wall
+    public static Vector2 ApplyBallPositionToMouse(Ball ball, Vector2 mousePosition, List<Wall> walls)
+    {
+        // --- Ball stuff ---
+        Vector2 dirBallToMouse = Vector2.Normalize(mousePosition - ball.position);
+        if(float.IsNaN(dirBallToMouse.X)) dirBallToMouse.X = 0;
+        if(float.IsNaN(dirBallToMouse.Y)) dirBallToMouse.Y = 0;
+        float distanceBallToMouse = Vector2.Distance(mousePosition, ball.position);
+
+        Vector2 normalForce = Vector2.Zero;
+        
+        foreach(Wall wall in walls)
+        {
+            float maxLeft = wall.vertices[0].X;
+            float maxRight = wall.vertices[0].X;
+            float maxUp = wall.vertices[0].Y;
+            float maxDown = wall.vertices[0].Y;
+
+            foreach(Vector2 vertex in wall.vertices)
+            {
+                if(vertex.X < maxLeft) maxLeft = vertex.X;
+                if(vertex.X > maxRight) maxRight = vertex.X;
+                if(vertex.Y < maxUp) maxUp = vertex.Y;
+                if(vertex.Y > maxDown) maxDown = vertex.Y;
+            }
+
+            float width = MathF.Abs(maxRight - maxLeft);
+            float height = MathF.Abs(maxDown - maxUp);
+            
+            Vector2 aabbCenter = new Vector2((maxLeft + maxRight) / 2, (maxUp + maxDown) / 2) + wall.position;
+
+            if(CheckAABB(ball.position, ball.radius * 2, ball.radius * 2, aabbCenter, width, height))
+            {
+                for(int i = 0; i < wall.vertices.Count(); i++)
+                {
+                    if(i < wall.vertices.Count() - 1)
+                    {
+                        Vector2 lineSegPointA = wall.vertices[i] + wall.position;
+                        Vector2 lineSegPointB = wall.vertices[i+1] + wall.position;
+                        // get reaction/normal force on ball from the wall
+                        // current wall, y = 0x - 20, -100 < x < 100 => 0x - y - 20 = 0
+                        float t = Vector2.Dot(ball.position - lineSegPointA, lineSegPointB - lineSegPointA) /
+                                    Vector2.Dot(lineSegPointB - lineSegPointA, lineSegPointB - lineSegPointA);
+
+                        t = Math.Clamp(t, 0, 1);
+
+                        Vector2 closestPoint = lineSegPointA + t * (lineSegPointB - lineSegPointA);
+                        Vector2 normal = Vector2.Normalize(ball.position - closestPoint);
+                        float distance = (ball.position - closestPoint).Length();
+                        float depth = ball.radius - distance;
+                        
+                        normalForce += normal * depth * 1000;
+                    }
+                }
+            }
+        }
+       
+
+        Vector2 force = ball.springConstant * dirBallToMouse * distanceBallToMouse - (ball.velocity * ball.dampingFactor);
+        force += normalForce;
+        Vector2 acceleration = force/ball.mass;
+
+        ball.velocity += acceleration * Raylib.GetFrameTime();
+        ball.position += ball.velocity * Raylib.GetFrameTime();
+        return normalForce;
+    }
+
+    public static bool CheckAABB(Vector2 centreA, float widthA, float heightA, Vector2 centreB, float widthB, float heightB)
+    {
+        float minX_A = centreA.X - widthA/2;
+        float maxX_A = centreA.X + widthA/2;
+
+        float minY_A = centreA.Y - heightA/2;
+        float maxY_A = centreA.Y + heightA/2;
+
+        float minX_B = centreB.X - widthB/2;
+        float maxX_B = centreB.X + widthB/2;
+
+        float minY_B = centreB.Y - heightB/2;
+        float maxY_B = centreB.Y + heightB/2;
+
+        if(maxX_A < minX_B) return false;
+        if(maxX_B < minX_A) return false;
+        if(maxY_A < minY_B) return false;
+        if(maxY_B < minY_A) return false;
+
+        return true;
+    }
+}
---
+++ HapticSim2D/Program.cs
@@ -0,0 +1,95 @@
+﻿using System.Numerics;
+using Raylib_cs;
+
+public class Program
+{
+    const int screenWidth = 800;
+    const int screenHeight = 600;
+    static Ball ball = new Ball(_position:new Vector2(0,0), _radius:20, 
+                                _mass:1, _springConstant:200, _dampingFactor:25, _colour:Color.Red);
+    static float offsetX = screenWidth / 2;
+    static float offsetY = screenHeight / 2;
+    static List<Wall> walls = new List<Wall>();
+
+    static void Start()
+    {
+        Raylib.InitWindow(screenWidth, screenHeight, "Haptic Simulation");
+        Raylib.SetTargetFPS(60);
+        walls.Add(new Wall(new Vector2(0, 20)));
+        walls[0].vertices = [new Vector2(-100, 0), new Vector2(100, 0)];
+    }
+
+    public static void Main()
+    {
+        Start();
+
+        while (!Raylib.WindowShouldClose())
+        {
+            Vector2 mousePosition = Raylib.GetMousePosition() - new Vector2(offsetX, offsetY);
+            Vector2 normalForce = Physics.ApplyBallPositionToMouse(ball, mousePosition, walls);
+            normalForce /= 1000;
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Color.LightGray);
+            Raylib.DrawCircle((int)(ball.position.X + offsetX), (int)(ball.position.Y+offsetY), ball.radius, ball.colour);
+            
+            foreach(Wall wall in walls)
+            {
+                for(int i = 0; i < wall.vertices.Count(); i++)
+                {
+                    int posX = (int)(wall.vertices[i].X + offsetX + wall.position.X);
+                    int posY = (int)(wall.vertices[i].Y + offsetY + wall.position.Y);
+                    Raylib.DrawCircle(posX, posY, 5f, Color.DarkGray);
+
+                    if(i < wall.vertices.Count() - 1)
+                    {
+                        int nextPosX = (int)(wall.vertices[i+1].X + offsetX + wall.position.X);
+                        int nextPosY = (int)(wall.vertices[i+1].Y + offsetY + wall.position.Y);
+
+                        Raylib.DrawLine(posX, posY, nextPosX, nextPosY, Color.DarkGray);
+                    }
+                }
+            }
+            
+            Raylib.DrawLine((int)(ball.position.X + offsetX), (int)(ball.position.Y + offsetY),
+                            (int)(ball.position.X + offsetX + normalForce.X), (int)(ball.position.Y + offsetY + normalForce.Y),
+                            Color.White);
+
+            Raylib.EndDrawing();   
+        }
+
+        Raylib.CloseWindow();
+    }
+}
+
+public class Ball
+{
+    public Vector2 position;
+    public Vector2 velocity;
+    public float radius;
+    public float mass;
+    public float springConstant;
+    public float dampingFactor;
+    public Color colour;
+    
+
+    public Ball(Vector2 _position, float _radius,  float _mass, float _springConstant, float _dampingFactor, Color _colour)
+    {
+        position = _position;
+        radius = _radius;
+        mass = _mass;
+        colour = _colour;
+        springConstant = _springConstant;
+        dampingFactor = _dampingFactor;
+    }
+}
+
+public class Wall
+{
+    public Vector2 position;
+    public Vector2[] vertices = new Vector2[0];
+
+    public Wall(Vector2 _position)
+    {
+        position = _position;
+    }
+}
